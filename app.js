260b31ed9033a30584cb7c00b126a5e085c16018
Cam Beaudoin <camilebeaudoin@gmail.com>
2017-02-11 14:22:50 -0500

    cloudant demo complete

--- app.js
+++ app.js
@@ -3,21 +3,37 @@ var Cloudant = require('cloudant');
 var options = require('./config/options');
 
 var login = {
-  username: options.storageConfig.username,
-  password: options.storageConfig.password,
-  host: options.storageConfig.host,
-  port: options.storageConfig.port,
-  url: options.storageConfig.url
+  username: options.config.username,
+  password: options.config.password,
+  host: options.config.host,
+  port: options.config.port,
+  url: options.config.url
 };
  
-var me = login.username;
-// var me = 'nodejs'; // Set this to your own account 
-var password = login.password;
-// var password = process.env.cloudant_password;
- 
 // Initialize the library with my account. 
-var cloudant = Cloudant({account:me, password:password});
+var cloudant = Cloudant({account:login.username, password:login.password});
  
 cloudant.db.list(function(err, allDbs) {
   console.log('All my databases: %s', allDbs.join(', '))
+});
+
+// Remove any existing database called "alice". 
+cloudant.db.destroy('alice', function(err) {
+ 
+  // Create a new "alice" database. 
+  cloudant.db.create('alice', function() {
+ 
+    // Specify the database we are going to use (alice)... 
+    var alice = cloudant.db.use('alice')
+ 
+    // ...and insert a document in it. 
+    alice.insert({ crazy: true }, 'rabbit', function(err, body, header) {
+      if (err) {
+        return console.log('[alice.insert] ', err.message);
+      }
+ 
+      console.log('You have inserted the rabbit.');
+      console.log(body);
+    });
+  });
 });
